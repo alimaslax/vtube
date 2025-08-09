d9400539c20d46a531571c65157da93660391f8a
Maslax Ali <mali@netjets.com>
2025-08-09 00:37:08 -0400

    Initial Commit

---
+++ VTube/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VTube
+//
+//  Created by Maslax Ali on 8/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VTube/VTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VTubeApp.swift
+//  VTube
+//
+//  Created by Maslax Ali on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct VTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
